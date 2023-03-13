62f771943053a1605191151aefa3947a557a0d3f
artem_bilozor <theartbi@gmail.com>
2023-03-13 23:39:59 +0200

    add WebDriverManager

--- src/test/java/io/artbi/automation/test_coverage/MainPageTest.java
+++ src/test/java/io/artbi/automation/test_coverage/MainPageTest.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.artbi.automation.test_coverage.webdriver.listener.MyLocatorListener;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
@@ -32,6 +33,8 @@ public class MainPageTest {
 
     @BeforeEach
     public void setUp() {
+
+        WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
