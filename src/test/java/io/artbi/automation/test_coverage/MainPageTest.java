b936a1fdcc96389520e7066c0a37132195384430
artem_bilozor <theartbi@gmail.com>
2023-03-14 17:23:58 +0200

    add remote options for webdriver

--- src/test/java/io/artbi/automation/test_coverage/MainPageTest.java
+++ src/test/java/io/artbi/automation/test_coverage/MainPageTest.java
@@ -1,10 +1,10 @@
 package io.artbi.automation.test_coverage;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.artbi.automation.test_coverage.webdriver.listener.MyLocatorListener;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
@@ -12,8 +12,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 
 import java.io.IOException;
@@ -28,19 +28,28 @@ public class MainPageTest {
 
     @BeforeAll
     public static void setUpAll() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
-    }
+        Configuration.driverManagerEnabled = false;
+        Configuration.remote = "http://localhost:4444";
+        Configuration.headless = true;
 
-    @BeforeEach
-    public void setUp() {
-
-        WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--headless");
 
-        WebDriver driver = new ChromeDriver(options);
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        Configuration.browserCapabilities = Configuration.browserCapabilities.merge(capabilities);
+
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @BeforeEach
+    public void setUp() {
+        open();
+        WebDriver driver = WebDriverRunner.getWebDriver();
+
         final EventFiringWebDriver eventFiringWebDriver = new EventFiringWebDriver(driver);
         locatorsListener = new MyLocatorListener();
         eventFiringWebDriver.register(locatorsListener);
